85bfdc6d94f6eb0d65a19869b1df5862378b822d
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2023-07-26 06:19:47 +0900

    rollback static file serve view

--- compactSharing/globalfiles/urls.py
+++ compactSharing/globalfiles/urls.py
@@ -1,7 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
 
+from .views import (
+    static, media, 
+)
 
-urlpatterns = []
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('static/<str:appname>/<str:filetype>/<str:filename>', static), 
+    path('media/<str:appname>/<str:filetype>/<str:filename>', media), 
+]
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
