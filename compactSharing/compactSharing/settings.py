930d57a5a1e56099db9f6fd2f9c918e35f789db9
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2023-07-28 20:06:01 +0900

    db as sqlite

--- compactSharing/compactSharing/settings.py
+++ compactSharing/compactSharing/settings.py
@@ -95,16 +95,16 @@ WSGI_APPLICATION = 'compactSharing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-DEBUG = True
 '''
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -115,7 +115,7 @@ DATABASES = {
         'PORT': os.environ["PGPORT"],
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
