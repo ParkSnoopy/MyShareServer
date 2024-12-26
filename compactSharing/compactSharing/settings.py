d1994b6136c4e504f9ea2f57b31dfc5533b5dc54
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-12-26 15:37:20 +0900

    merge to main (#5)
    * db as sqlite
    * resolve conflict of settings.py

--- compactSharing/compactSharing/settings.py
+++ compactSharing/compactSharing/settings.py
@@ -108,11 +108,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
 
