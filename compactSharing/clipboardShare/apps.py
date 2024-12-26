8b1ae5a02b4277c6f2f93c364383f0a46862e16b
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-12-26 15:53:24 +0900

    merge with minimal breakings

--- compactSharing/clipboardShare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClipboardshareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clipboardShare'
--- compactSharing/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import (
-    homepage
-)
-
-
-urlpatterns = [
-    path('', homepage), 
-]
